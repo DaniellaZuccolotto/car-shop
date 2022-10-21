6ccadaf6fbdeaa1fdaec17f6f1829996dd040558
Daniella Zuccolotto <danielaz_9@hotmail.com>
2022-10-21 14:58:55 -0300

    feat: Fiz o car Model, car controller, mongoModel

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/error';
-import carRouter from './routes/carRouter';
+import carRouter from './routes/CarRouter';
 
 const app = express();
 app.use(express.json());
--- src/routes/CarRouter.ts
+++ src/routes/CarRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import CarController from '../controllers/Car';
-import CarModel from '../models/Car';
-import CarService from '../services/carService';
+import CarController from '../controllers/CarController';
+import CarModel from '../models/CarModel';
+import CarService from '../services/CarService';
 
 const route = Router();
 
